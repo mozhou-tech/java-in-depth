05766c73459d57e7142ed30f379d5fb3c50cac87
liuyuancheng <tenstone@foxmail.com>
2021-07-11 19:58:38 +0800

    实现3种单例模式

---
+++ patterns/src/main/java/com/tenstone/leet/patterns/singleton/static_inner_class/Singleton.java
@@ -0,0 +1,23 @@
+package com.tenstone.leet.patterns.singleton.static_inner_class;
+
+/**
+ * Created by liuyuancheng on 2021/7/11  <br/>
+ */
+public class Singleton {
+
+    private Singleton() {
+
+    }
+
+    /**
+     * 静态内部类单例模式也称单例持有者模式，实例由内部类创建，由于 JVM 在加载外部类的过程中, 是不会加载静态内部类的,
+     * 只有内部类的属性/方法被调用时才会被加载, 并初始化其静态属性。
+     */
+    private static class InstanceHolder {
+        private final static Singleton instance = new Singleton();
+    }
+
+    public static Singleton getSingleton() {
+        return InstanceHolder.instance;
+    }
+}
