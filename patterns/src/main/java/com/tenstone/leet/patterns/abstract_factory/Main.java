86ef092317f265deb04f12c1dab6772014e8204d
liuyuancheng <tenstone@foxmail.com>
2021-07-14 22:46:07 +0800

    抽象工厂方法

---
+++ patterns/src/main/java/com/tenstone/leet/patterns/abstract_factory/Main.java
@@ -0,0 +1,10 @@
+package com.tenstone.leet.patterns.abstract_factory;
+
+/**
+ * Created by liuyuancheng on 2021/7/14  <br/>
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
