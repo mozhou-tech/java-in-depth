a9993a2f1acf94525143ffea3a2f887f626e434d
liuyuancheng <tenstone@foxmail.com>
2021-07-17 21:43:46 +0800

    策略模式

---
+++ patterns/src/main/java/com/tenstone/leet/patterns/strategy/Strategy.java
@@ -0,0 +1,12 @@
+package com.tenstone.leet.patterns.strategy;
+
+/**
+ * 策略接口
+ *
+ * Created by liuyuancheng on 2021/7/17  <br/>
+ */
+public interface Strategy {
+    Hand nextHand();
+
+    void study(boolean win);
+}
