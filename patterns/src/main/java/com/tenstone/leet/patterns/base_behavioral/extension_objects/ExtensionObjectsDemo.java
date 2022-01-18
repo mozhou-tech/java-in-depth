16c30be1707f5e34b9882b02973d57e874f75a11
liuyuancheng <tenstone@foxmail.com>
2022-01-18 09:51:11 +0800

    extension objects pattern

---
+++ patterns/src/main/java/com/tenstone/leet/patterns/base_behavioral/extension_objects/ExtensionObjectsDemo.java
@@ -0,0 +1,11 @@
+package com.tenstone.leet.patterns.base_behavioral.extension_objects;
+
+/**
+ * Created by liuyuancheng on 2022/1/18  <br/>
+ * 扩展对象模式
+ *
+ *
+ * @author liuyuancheng
+ */
+public class ExtensionObjectsDemo {
+}
---
+++ patterns/src/main/java/com/tenstone/leet/patterns/base_structural/role_object/RoleObjectDemo.java
@@ -0,0 +1,9 @@
+package com.tenstone.leet.patterns.base_structural.role_object;
+
+/**
+ * Created by liuyuancheng on 2022/1/18  <br/>
+ *
+ * @author liuyuancheng
+ */
+public class RoleObjectDemo {
+}
