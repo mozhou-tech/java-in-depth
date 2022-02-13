5f3bbe50933dc28bbb2139505136df1043f5d840
liuyuancheng <tenstone@foxmail.com>
2022-02-13 11:33:47 +0800

    [jdk] threadpool

--- jdk/src/main/java/com/tenstone/jdk/multithread/ThreadPoolDemo.java
+++ jdk/src/main/java/com/tenstone/jdk/multithread/ThreadPoolDemo.java
@@ -1,7 +1,38 @@
 package com.tenstone.jdk.multithread;
 
+import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 /**
  * Created by liuyuancheng on 2021/7/8  <br/>
  */
 public class ThreadPoolDemo {
+
+    @Test
+    void main() {
+        ThreadPoolExecutor executor =
+                (ThreadPoolExecutor) Executors.newFixedThreadPool(2);
+        executor.submit(() -> {
+            Thread.sleep(1000);
+            return null;
+        });
+        executor.submit(() -> {
+            Thread.sleep(1000);
+            return null;
+        });
+        executor.submit(() -> {
+            Thread.sleep(1000);
+            return null;
+        });
+
+        assertEquals(2, executor.getPoolSize());
+        assertEquals(1, executor.getQueue().size());
+        executor.shutdownNow();
+        System.out.println(executor.isShutdown());
+    }
+
 }
