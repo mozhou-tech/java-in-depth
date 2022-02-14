bf4f08a009c170a23dff734112905aa93d1d8c5c
liuyuancheng <tenstone@foxmail.com>
2022-02-14 17:42:32 +0800

    [flink] flink modules modified

--- apache-flink/flink-project/src/main/java/com/imooc/flink/utils/StringUtils.java
+++
@@ -1,6 +0,0 @@
-package com.imooc.flink.utils;
-
-public class StringUtils {
-
-    public static final String GAODE_KEY = "";
-}
---
+++ apache-flink/flink-project/src/main/java/com/tenstone/flink/datastream/utils/StringUtils.java
@@ -0,0 +1,6 @@
+package com.tenstone.flink.datastream.utils;
+
+public class StringUtils {
+
+    public static final String GAODE_KEY = "";
+}
