95b702c3059c59c4cd1e661661954d79a6a1b8b3
liuyuancheng <tenstone@foxmail.com>
2022-02-25 15:05:00 +0800

    [swordoffer] Q30

--- architecture-algorithms/src/main/java/com/tenstone/leet/algorithm/swordoffer/Q30_MinStack.java
+++ architecture-algorithms/src/main/java/com/tenstone/leet/algorithm/swordoffer/Q30_MinStack.java
@@ -2,11 +2,14 @@ package com.tenstone.leet.algorithm.swordoffer;
 
 import org.junit.jupiter.api.Assertions;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Stack;
 
 /**
  * Created by liuyuancheng on 2022/2/25  <br/>
+ * <p>
+ * 最小栈 先写操作流程注释再写代码
+ * Stack底层基于Vector，可以使用Vector提供的方法
+ * https://leetcode-cn.com/problems/bao-han-minhan-shu-de-zhan-lcof/
  *
  * @author liuyuancheng
  */
@@ -14,39 +17,44 @@ public class Q30_MinStack {
 
     static class MinStack {
 
-        private int size;
+        private Stack<Integer> a = new Stack<>();
 
-        private List<Integer> minValueList = new ArrayList<>();
-
-        private List<Integer> store = new ArrayList<>();
+        /**
+         * 辅助栈，从上往下升序排列
+         */
+        private Stack<Integer> b = new Stack<>();
 
         public MinStack() {
 
         }
 
         public void push(int x) {
-            store.add(x);
-            if (minValueList.isEmpty()) {
-                minValueList.add(x);
+            a.push(x);
+            if (b.isEmpty() || b.peek() >= x) {
+                b.push(x);
             } else {
-                minValueList.add(Math.min(minValueList.get(size - 1), x));
+                int stackSize = b.size();
+                for (int i = stackSize - 1; i >= 0; i--) {
+                    if (b.get(i) >= x) {
+                        b.insertElementAt(x, i);
+                        // 记得要跳出循环
+                        break;
+                    }
+                }
             }
-            size++;
         }
 
         public void pop() {
-            int popValue = store.remove(size - 1);
-            size--;
-            // 删除第一次匹配的元素
-            minValueList.remove(Integer.valueOf(popValue));
+            int popVal = a.pop();
+            b.removeElement(popVal);
         }
 
         public int top() {
-            return store.get(size - 1);
+            return a.peek();
         }
 
         public int min() {
-            return minValueList.get(size - 1);
+            return b.peek();
         }
     }
 
@@ -56,11 +64,15 @@ public class Q30_MinStack {
         minStack.push(-1024);
         minStack.push(-1024);
         minStack.push(512);
+        System.out.println(minStack.a);
+        System.out.println(minStack.b);
         minStack.pop();
         Assertions.assertEquals(-1024, minStack.min());
         minStack.pop();
         Assertions.assertEquals(-1024, minStack.min());
         minStack.pop();
         Assertions.assertEquals(512, minStack.min());
+        Assertions.assertEquals(512, minStack.top());
+        //====================================
     }
 }
