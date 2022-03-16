fdc3e839cddd68a6a6a867b8427ce995fd87916d
liuyuancheng <tenstone@foxmail.com>
2022-03-16 18:48:47 +0800

    rust demo

---
+++ rustpra/src/main.rs
@@ -0,0 +1,12 @@
+use std::collections::HashMap;
+use ferris_says::say; // from the previous step
+use std::io::{stdout, BufWriter};
+use serde_json::to_string;
+
+fn main() {
+    let stdout = stdout();
+    let message = String::from("Hello World!");
+    let width = message.chars().count();
+    let mut writer = BufWriter::new(stdout.lock());
+    say(message.as_bytes(), width, &mut writer).unwrap();
+}
